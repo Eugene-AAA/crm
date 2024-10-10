c81c5e81e74705706e406283f7a066675330cf3a
Калиничев Евгений <bloodyoverstep@gmail.com>
2024-10-10 21:33:44 +0400

    initial

---
+++ src/main/java/ru/hr/crm/dto/ApplicantDto.java
@@ -0,0 +1,13 @@
+package ru.hr.crm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApplicantDto {
+    private String name;
+    private int age;
+}
